553e3cba329499f01337e2c79294099d1bd7de0a
PhongNguyen2017 <phongnguyen1605@gmail.com>
2017-10-18 19:37:00 +0700

    ver2

---
+++ app/routes/route.js
@@ -0,0 +1,285 @@
+var mongoose = require('mongoose'), ejs = require('ejs')
+
+var Product = require('../Models/product.js');
+
+var User = require('../Models/user.js');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var express=require('express');
+
+module.exports = function(app) {
+
+
+    app.get('/Product', (req, res) => {
+        ejs.renderFile('./view/Product.ejs', {}, (err, html) => {
+            res.end(html);
+        });
+    });
+
+
+    app.get('/ProductList', function(req, res) {
+
+     
+
+        //----------HAM TIM KIEM THEO TEN------------------------//
+        
+        if(req.query.search && req.query.search.length > 0){
+        
+         Product.find({name: {$regex: ".*" + req.query.search + "*"}}, function(err, products) {
+             if (err)
+                  res.send(err);
+                    ejs.renderFile('./view/ProductList.ejs', {products}, (err, html) => {
+                         res.end(html);
+                           });
+                      });
+            return ;
+            
+
+        }
+
+        //-----------------HAM TIM KIEM THEO KHOANG GIA --------------------------//
+
+
+
+
+        if(req.query.minprice && req.query.maxprice){
+            console.log('chay dc if seach');
+
+            var queryFP = req.query.minprice;
+            var queryLP = req.query.maxprice;
+
+
+            console.log('start find');
+            Product.find({}).where('price').gt(queryFP-1).lt(queryLP+1).exec((err, products)=> {
+              
+                console.log('khong co loi nhung ko chay dc')
+                ejs.renderFile('./view/ProductList.ejs', {products }, (err, html) => {
+                    res.end(html);
+                });
+
+            });
+          
+
+
+
+        }
+
+
+        
+
+            console.log('ko chay dc if');
+            Product.find(function(err,products){
+                ejs.renderFile('./view/ProductList.ejs',{products},(err, html) => {
+                    res.end(html)
+                })
+            })  
+
+    
+
+        });
+
+
+
+
+
+    //---------------------------------POST Product---------------------------------------------
+
+
+
+    app.post('/Product', (req, res,done) => {
+        const tmp = req.body;
+        var errMes = "";
+
+        if (!tmp.name) {
+            errMes += " Enter Product's Name. Please !!!";
+
+        }
+        if (!tmp.description) {
+            errMes += " Enter Product's description. Please!!! ";
+        }
+        if (!tmp.price) {
+            errMes += " Enter Product's price. Please!!! ";
+        }
+        if (errMes) {
+            ejs.renderFile('./view/Product.ejs', { errMes: errMes }, (err, html) => {
+                res.end(html);
+            });
+        }
+
+         else {
+            var newProduct = new Product();
+            newProduct.name = req.body.name;
+            newProduct.description = req.body.description;
+            newProduct.price = req.body.price;
+            newProduct.save(function(err){
+        if (err)
+        return ejs.renderFile('./view/Product.ejs', { errMes: err.message }, (err, html) => {
+                res.end(html);
+            });
+
+      res.redirect('/Product');
+            })
+        }
+        
+    });
+
+
+
+  //----------------Ham Delete Product theo ID-----------------------//  
+
+
+app.delete('/ProductList/:_id', function(req, res){
+    Product.findByIdAndRemove({_id: req.params._id}, 
+       function(err, docs){
+        if(err) res.json(err);
+        else    res.redirect('/ProductList');
+    });
+});
+
+//---------------------------------------------------//
+
+
+
+
+app.get('/', function(req, res, next) {
+    Product.find(function (err, result) {
+      if (err) return next(err);
+      res.json(result);
+    });
+  });
+
+
+//------------------------------------------
+
+
+app.get('/list', function(req, res, next) {
+    Product.find(function (err, result) {
+      if (err) return next(err);
+       ejs.renderFile('./view/ProductList.ejs', {result: result}, (err, html) => {
+            res.end(html);
+        });
+    });
+  });
+
+
+//------------------------- User Module - Phong -----------------------
+
+// Get Homepage
+app.get('/', ensureAuthenticated, function(req, res){
+	res.render('index');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/route/login');
+	}
+}
+  // Register
+app.get('/register', function(req, res){
+	res.render('register');
+});
+
+// Login
+app.get('/login', function(req, res){
+	res.render('login');
+});
+
+// Register User
+app.post('/register', function(req, res){
+	var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('phone','Phone number is required').notEmpty();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+            email:email,
+            phone:phone,
+			username: username,
+            password: password,
+            
+		});
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+
+		req.flash('success_msg', 'You are registered and can now login');
+
+		res.redirect('/route/login');
+	}
+});
+// Authentication
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   User.getUserByUsername(username, function(err, user){
+   	if(err) throw err;
+   	if(!user){
+   		return done(null, false, {message: 'Unknown User'});
+   	}
+
+   	User.comparePassword(password, user.password, function(err, isMatch){
+   		if(err) throw err;
+   		if(isMatch){
+   			return done(null, user);
+   		} else {
+   			return done(null, false, {message: 'Invalid password'});
+   		}
+   	});
+   });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.post('/login',
+  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/route/login',failureFlash: true}),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+	req.logout();
+
+	req.flash('success_msg', 'You are logged out');
+
+	res.redirect('/route/login');
+});
+
+
+
+
+};
+
+
+
---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+var methodOverride = require('method-override');
+
+// Component for User module - Phong
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+app = express()
+	  express_session = require('express-session'),
+	  bodyParser = require('body-parser'),
+	  port = process.env.PORT || 8080,
+	  mongoose = require('mongoose'),
+	  ejs = require('ejs'),
+      product = require('./app/Models/product.js'),
+      session = require('express-session'),
+      cookieParser = require('cookie-parser');
+
+
+
+
+
+mongoose.Promise = global.Promise;
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'iloveyoubaobao', // session secret
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(methodOverride('_method'))
+
+var routes = require('./app/routes/route.js'); //importing route
+routes(app);
+
+// Passport init - Phong
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator - Phong
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash - Phong
+app.use(flash());
+
+// Global Vars - Phong
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+app.get('/mydemo', (req, res)=>
+{
+	res.send('Hello World!');
+});
+
+app.listen(port,()=>
+{
+	console.log('listened port'+ port);
+});
+
